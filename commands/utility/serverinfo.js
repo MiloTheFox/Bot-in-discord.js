7f2a5b1f0db7d16ca7c43f05fa5873957ddef64c
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-03-31 23:24:41 +0200

    Create serverinfo.js

---
+++ commands/utility/serverinfo.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'serverinfo',
+    description: 'Gets information about the server.',
+    usage: 'ds!serverinfo',
+    run: async(client, msg, args) => {
+        try {
+        const serverinfo = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${msg.guild.name} Server Information`)
+            .setThumbnail(msg.guild.iconURL({dynamic: true}))
+            .addField('Server Name', `${msg.guild.name}`)
+            .addField('Server ID', `${msg.guild.id}`)
+            .addField('Server Creation Date', `**<t:${Math.round(msg.guild.createdAt.getTime() / 1000, 0)}:F>**`)
+            .addField('Server Owner', `<@!${msg.guild.ownerId}> - \`(${msg.guild.ownerId})\``)	
+            .addField('Server Region', `${msg.guild.region}`)
+            .addField('Server Members', `${msg.guild.memberCount}`)
+            .addField('Server Channels', `${msg.guild.channels.cache.size}`)
+            .addField('Server Roles', `${msg.guild.roles.cache.size}`)
+            .setTimestamp();
+        return msg.channel.send({embeds: [serverinfo]});
+        } catch (error) {
+            console.log(error);
+            return msg.channel.send('An error occurred! Please try again!');
+        }
+    }
+};
