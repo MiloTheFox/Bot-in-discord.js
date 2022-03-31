37d1234528e829396b19671a767cec7e436a32e9
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-03-31 23:19:10 +0200

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -6,14 +6,12 @@ module.exports = {
     description: 'Gets all bot commands.',
     usage: 'ds!help',
     run: async(client, msg, args) => {
-        // initialize the help command
         try {
             const help = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Help Command')
                 .setDescription(`**Commands**\n\`\`\`fix\n${client.commands.map(cmd => `${cmd.name} - ${cmd.description}`).join('\n')}\`\`\`\n\n**Commands Usage**\n\`\`\`fix\n${client.commands.map(cmd => `${cmd.name} - ${cmd.usage}`).join('\n')}\`\`\``)
                 .setTimestamp();
-            // send the embed
             return msg.channel.send({embeds: [help]});
         } catch (error) {
             console.log(error);
