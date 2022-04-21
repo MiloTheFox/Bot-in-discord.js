6808a6b1c763bb24aeb512df41b39f09b69c2c15
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-04-21 18:07:53 +0200

    Update exec.js

--- commands/developer/exec.js
+++ commands/developer/exec.js
@@ -1,22 +1,24 @@
 const { MessageEmbed } = require('discord.js');
 const { exec } = require('child_process');
 
+
 module.exports = {
     name: 'exec',
     description: 'Executes a command in the terminal.',
-    usage: 'ds!exec <linux-command>',
+    aliases: ['exec_command', 'exec_cmd', 'execute_command', 'execute_cmd', 'run'],
+    usage: 'ds!exec <command>',
     run: async (client, msg, args) => {
         try{
-        // check if the user is the bot owner
             if (msg.author.id !== '705557092802625576') {
                 msg.channel.send('You are not the Bot Owner!');
                 return;
             }
-            // check if the user provided a command
+            
             if (args.length < 1) {
-                msg.channel.send('You need to provide a Command to execute!');
+                msg.channel.send('You need to provide some code!');
                 return;
             }
+
             exec(args.join(' '), (error, stdout, stderr) => {
                 if (!error) {
                     const embed = new MessageEmbed()
@@ -26,6 +28,7 @@ module.exports = {
                     .setThumbnail(msg.author.displayAvatarURL({dynamic: true}))
                     .setDescription(`**Input:** \`\`\`js\n${args.join(' ')}\n\`\`\`\n**Output:**\`\`\`js\n${stdout}\n\`\`\``)
                     .setTimestamp();
+                    // send the embed
                     return msg.reply({embeds: [embed]});
                 } else {
                     const embed = new MessageEmbed()
