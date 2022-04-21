cfa87a070d568838f4f971d0f1d3606386862e1c
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-04-21 18:25:53 +0200

    Create pause.js

---
+++ commands/music/pause.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: 'pause',
+    description: 'Pauses the music.',
+    usage: 'ds!pause',
+    run: async(client, msg, args) => {
+        try{
+            if(!msg.member.permissions.has('CONNECT')) {
+                msg.channel.send('You do not have the permission to use this command!');
+                return;
+            }
+            if (!msg.guild.me.permissions.has('SPEAK')) {
+                msg.channel.send('I do not have the permission to use this command!');
+                return;
+            }
+            if (!client.queue[msg.guild.id]) {
+                msg.channel.send('There is nothing playing!');
+                return;
+            }
+            if(client.queue[msg.guild.id].playing) {
+                client.queue[msg.guild.id].playing = false;
+                return msg.channel.send('Music paused!');
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    }
+};
