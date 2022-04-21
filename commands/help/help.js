9e46dd06f89fa83ca9d9d29dad4acad030c88aad
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-04-21 18:11:22 +0200

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,21 +1,46 @@
-const Discord = require('discord.js');
-const moment = require('moment');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: 'help',
-    description: 'Gets all bot commands.',
-    usage: 'ds!help',
-    run: async(client, msg, args) => {
-        try {
-            const help = new Discord.MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Help Command')
-                .setDescription(`**Commands**\n\`\`\`fix\n${client.commands.map(cmd => `${cmd.name} - ${cmd.description}`).join('\n')}\`\`\`\n\n**Commands Usage**\n\`\`\`fix\n${client.commands.map(cmd => `${cmd.name} - ${cmd.usage}`).join('\n')}\`\`\``)
-                .setTimestamp();
-            return msg.channel.send({embeds: [help]});
-        } catch (error) {
-            console.log(error);
-            return msg.channel.send('An error occurred! Please try again!');
+  name: "help",
+  aliases: ["h"],
+  showHelp: false,
+  usage: "{prefix}help",
+
+  run: async(client, message, args) => {
+    try {
+      const embed = new MessageEmbed();
+
+      embed.setColor("BLUE");
+      embed.setTitle(client.user.username);
+      embed.setThumbnail(client.user.displayAvatarURL());
+      const commands = client.commands.filter((x) => x.showHelp !== false);
+      if (args.length === 0) {
+        embed.setDescription(
+          "**Commands:**\n" +
+            commands
+            .map((x) => `\`(${x.name})\` - ${x.description}`)
+            .join("\n")
+        );
+      } else {
+        // print the help for the command that is provided
+        const command = commands.find((x) => x.name === args[0]);
+        if (command) {
+          embed.setDescription(
+            `**Command:** \`(${command.name})\`\n**Aliases:** [${command.aliases}]\n**Description:** ${command.description}\n**Utilisation:** ${command.usage}`
+          );
+        } else {
+          embed.setDescription(
+            `**Command:** \`(${args[0]})\`\n**Description:** Command not found!`
+          );
         }
+      }
+      embed.setTimestamp();
+      embed.setFooter({
+        iconURL: message.author.avatarURL({ dynamic: true }),
+      });
+      message.channel.send({ embeds: [embed] });
+    } catch (error) {
+      console.log(error);
     }
+  },
 };
