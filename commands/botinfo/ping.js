b03146eb8f1edc7f8c50fd815f84a29b928764a9
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-04-30 10:24:30 +0200

    Update ping.js

--- commands/botinfo/ping.js
+++ commands/botinfo/ping.js
@@ -1,10 +1,26 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
     name: 'ping',
+    aliases: ["latency"],
     description: 'Returns the Bot\'s latency!',
-    usage: "ds!ping",
-    aliases: ['latency'],
     run: async (client, message, args) => {
-        const msg = await message.channel.send('Pinging...');
-        msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ws.ping)}ms`);
+        try{
+        const start = Date.now();
+        message.channel.send('Ping!').then(m => {
+            const embed = new MessageEmbed()
+                .setColor('BLUE')
+                .setTitle(client.user.username + " - Pong!")
+                .setThumbnail(client.user.displayAvatarURL())
+                .addField(`Message Ping`, `\`${Date.now() - start}ms\` 🛰️`)
+                .addField(`Message Latency`, `\`${m.createdTimestamp - start}ms\` 🛰️`)
+                .addField(`API Latency`, `\`${Math.round(client.ws.ping)}ms\` 🛰️`)
+                .setTimestamp()
+                .setFooter({ text: `Made by ${client.users.cache.get('705557092802625576').tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) });
+            m.edit({ embeds: [embed] });
+        });
+        } catch(error){
+            console.log(error);
+        }
     }
 };
