65fdfeea1464e64985ecacdce40a15f3fe22d76c
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-05-19 20:41:44 +0200

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,46 +1,67 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js');
 
 module.exports = {
-  name: "help",
-  aliases: ["h"],
-  showHelp: false,
-  usage: "{prefix}help",
-
-  run: async(client, message, args) => {
-    try {
-      const embed = new MessageEmbed();
-
-      embed.setColor("BLUE");
-      embed.setTitle(client.user.username);
-      embed.setThumbnail(client.user.displayAvatarURL());
-      const commands = client.commands.filter((x) => x.showHelp !== false);
-      if (args.length === 0) {
-        embed.setDescription(
-          "**Commands:**\n" +
-            commands
-            .map((x) => `\`(${x.name})\` - ${x.description}`)
-            .join("\n")
-        );
-      } else {
-        // print the help for the command that is provided
-        const command = commands.find((x) => x.name === args[0]);
-        if (command) {
-          embed.setDescription(
-            `**Command:** \`(${command.name})\`\n**Aliases:** [${command.aliases}]\n**Description:** ${command.description}\n**Utilisation:** ${command.usage}`
-          );
-        } else {
-          embed.setDescription(
-            `**Command:** \`(${args[0]})\`\n**Description:** Command not found!`
-          );
+    name: 'help',
+    description: 'Gets information about the bot.',
+    usage: 'ds!help',
+    run: async(client, msg, args) => {
+        try {
+            switch (args[0]) {
+                case 'commands' || 'cmds':
+                    const commands = client.commands.map(cmd => `${cmd.name} - ${cmd.description}`).join('\n');
+                    const commandsEmbed = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle('Commands')
+                        .setDescription(`${commands}`)
+                        .setFooter({text: 'ds!help command [commandname] for more info on a command.'})
+                        .setTimestamp();
+                    return msg.channel.send({embeds: [commandsEmbed]});
+                case 'aliases':
+                    const aliases = [];
+                    client.commands.forEach(cmd => {
+                        if (cmd.aliases) {
+                            for (let i = 0; i < cmd.aliases.length; i++) {
+                                aliases.push(`${cmd.name} - ${cmd.aliases[i]}`);
+                            }
+                        }
+                        else {
+                            aliases.push(`${cmd.name} - None`);
+                        }
+                    });
+                    const aliasesEmbed = new Discord.MessageEmbed()	
+                        .setColor('#0099ff')
+                        .setTitle('Command Aliases')
+                        .setDescription(aliases.join('\n'))
+                        .setFooter({text: 'ds!help command [commandname] for more info on a command.'})	
+                        .setTimestamp();
+                    return msg.channel.send({embeds: [aliasesEmbed]});
+                case 'command' || 'cmd':
+                    const commandName = args[1];
+                    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+                    if (!command) return msg.channel.send('That command does not exist!');
+                    const commandEmbed = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle(`${command.name}`)
+                        .setDescription(`${command.description}`)
+                        .addField('Usage', `${command.usage}`)
+                        .addField('Aliases', `${command.aliases ? command.aliases.join(', ') : 'None'}`)
+                        .setFooter({text: 'ds!help command [commandname] for more info on a command.'})
+                        .setTimestamp();
+                    return msg.channel.send({embeds: [commandEmbed]});
+                default:
+                    const helpEmbed = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle('Help')
+                        .setDescription('Here are all the commands you can use!')
+                        .addField('Commands', 'ds!help commands')
+                        .addField('Aliases', 'ds!help aliases')
+                        .setFooter({text: 'ds!help command [commandname] for more info on a command.'})
+                        .setTimestamp();
+                    return msg.channel.send({embeds: [helpEmbed]});
+            }
+        } catch (error) {
+            console.log(error);
+            return msg.channel.send('An error occurred! Please try again!');
         }
-      }
-      embed.setTimestamp();
-      embed.setFooter({
-        iconURL: message.author.avatarURL({ dynamic: true }),
-      });
-      message.channel.send({ embeds: [embed] });
-    } catch (error) {
-      console.log(error);
     }
-  },
 };
