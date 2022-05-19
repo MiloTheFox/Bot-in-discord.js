d8799fa4f23ad71cf70f8db0940ca79e677edb85
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-05-19 20:37:25 +0200

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+module.exports = (client, int) => {
+  try {
+    if (!int.isButton()) return;
+
+    const queue = client.player.getQueue(int.guildId);
+    switch (int.customId) {
+      case "saveTrack":
+        {
+          if (!queue || !queue.playing)
+            return int.reply({
+              content: `No music is currently playing. ❌`,
+              ephemeral: true,
+              components: [],
+            });
+
+          const embed = new Discord.MessageEmbed()
+            .setColor("BLUE")
+            .setTitle(client.user.username + " - Save Track")
+            .setThumbnail(client.user.displayAvatarURL())
+            .addField(`Track`, `\`${queue.current.title}\``)
+            .addField(`Duration`, `\`${queue.current.duration}\``)
+            .addField(`URL`, `${queue.current.url}`)
+            .addField(`Saved Server`, `\`${int.guild.name}\``)
+            .addField(`Requested By`, `${queue.current.requestedBy}`)
+            .setTimestamp()
+            .setFooter({
+              text: "Music Utility",
+              iconURL: int.user.displayAvatarURL({ dynamic: true }),
+            });
+          int.member
+            .send({ embeds: [embed] })
+            .then(() => {
+              return int.reply({
+                content: `I sent you the name of the music in a private message ✅`,
+                ephemeral: true,
+                components: [],
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+              return int.reply({
+                content: `I cannot send you a private message. ❌`,
+                ephemeral: true,
+                components: [],
+              });
+            });
+        }
+        break;
+      case "time": {
+        if (!queue || !queue.playing)
+          return int.reply({
+            content: `No music is currently playing. ❌`,
+            ephemeral: true,
+            components: [],
+          });
+
+        const progress = queue.createProgressBar();
+        const timestamp = queue.getPlayerTimestamp();
+
+        if (timestamp.progress == "Infinity")
+          return int.message.edit({
+            content: `This song is live streaming, no duration data to display. 🎧`,
+          });
+
+        const embed = new Discord.MessageEmbed()
+          .setColor("BLUE")
+          .setTitle(queue.current.title)
+          .setThumbnail(client.user.displayAvatarURL())
+          .setTimestamp()
+          .setDescription(`${progress} **(${timestamp.progress}%)**`)
+          .setFooter({
+            text: "Dragon Serengeti",
+            iconURL: int.user.displayAvatarURL({ dynamic: true }),
+          });
+        int.message.edit({ content: ["Updated!"], embeds: [embed] });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
