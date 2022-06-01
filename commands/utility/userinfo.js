043dae4157668822cbccc0e75c2ca1014c469f49
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-06-01 11:42:52 +0200

    Updated userinfo.js
    Changed it cuz I'm bored lol

--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -1,60 +1,46 @@
 const log = (arg) => console.log(arg);
-// import the necessary modules
-const Discord = require('discord.js');
-const moment = require('moment');
 
-const status = {
-    online: "Online",
-    idle: "Idle",
-    dnd: "Do Not Disturb",
-    offline: "Offline/Invisible"
-};
+// import the necessary modules
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'userinfo',
     description: 'Gets information about a user.',
     usage: 'ds!userinfo <user>',
     aliases: ['user', 'u-info', 'uinfo', 'user-info'],
-    run: async(client, msg, args) => {
-        try{
-            const toTimestamp = (strDate) => {  
-                const dt = moment(strDate).unix();  
-                return dt;
-            };
-            let user = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) || msg.member;
-            if(!user) {
-                    let fetching = await client.users.fetch(args[0]);
-                    if (!fetching) {
-                        return msg.channel.send('Unable to find user! Check the ID and try again!');
-                    }
-                    const embed = new Discord.MessageEmbed()
-                        .setColor('GREEN')
-                        .setThumbnail(fetching.displayAvatarURL({dynamic: true}))
-                        .setAuthor({name: msg.author.tag, iconURL: msg.author.displayAvatarURL({dynamic: true})})
-                        .setTitle('User-Info')
-                        .addFields(
-                            {name: 'Username + Discriminator:', value: `${fetching.tag}`, inline: true},
-                            {name: 'Created at:', value: `**<t:${toTimestamp(moment.utc(fetching.createdTimestamp))}:F>**`, inline: true},
-                            {name: 'User-ID:', value: `${fetching.id}`, inline: true},
-                        )
+    run: async (client, msg, args) => {
+        try {
+            let member = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) || msg.guild.members.cache.find(member => member.user.username.toLowerCase() === args.slice(0).join(' ').toLowerCase());
+            if (member) {
+                try {
+                    const userInfo = new MessageEmbed()
+                        .setAuthor({ name: `${member.user.tag}`, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
+                        .setColor('BLUE')
+                        .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+                        .setTitle('User Info')
+                        .setDescription(`**Tag:** ${member.user.tag}\n**ID:** ${member.user.id}\n**Bot:** ${member.user.bot ? "✅" : "❌"}\n**Created At: <t:${Math.round(parseInt(member.user.createdTimestamp) / 1000)}:F>**\n**Joined At: <t:${Math.round(parseInt(member.joinedTimestamp) / 1000)}:F>**`)
                         .setTimestamp();
-                        return msg.channel.send({embeds: [embed]});
-                    }
-            const embed = new Discord.MessageEmbed()
-                .setColor('GREEN')
-                .setThumbnail(user.displayAvatarURL({dynamic: true}))
-                .setAuthor({name:  `${msg.author.tag}`, iconURL: user.displayAvatarURL({dynamic: true})})
-                .setTitle('User-Info')
-                .addFields(
-                    {name: 'Joined at:', value: `**<t:${toTimestamp(moment.utc(user.joinedTimestamp))}:F>**`, inline: true},
-                    {name: 'Created at:', value: `**<t:${toTimestamp(moment.utc(user.createdTimestamp))}:F>**`, inline: true},
-                    {name: 'User ID:', value: user.id, inline: true},
-                    {name: 'Highest Role:', value: user.roles.highest.name, inline: true},
-                    {name: 'Status:', value: `${status[user.presence.status]}` , inline: true},
-                    {name: 'Game:', value: `${user.presence.activities[0]}` ? `${user.presence.activities[0].state}` : `This Member doesn't play anything at the moment!`, inline: true},
-                )
-                .setTimestamp();
-            return msg.channel.send({embeds: [embed]});
-        } catch(error) {log(error);}
+                    return msg.channel.send({ embeds: [userInfo] });
+                } catch (err) {
+                    log(err);
+                    return
+                }
+            } else {
+                try {
+                    let fetching = await client.users.fetch(args[0]);
+                    const userInfo = new MessageEmbed()
+                        .setAuthor({ name: `${fetching.tag}`, iconURL: fetching.avatarURL({ dynamic: true }) })
+                        .setColor('BLUE')
+                        .setThumbnail(fetching.displayAvatarURL({ dynamic: true }))
+                        .setTitle('User Info')
+                        .setDescription(`**Tag:** ${fetching.tag}\n**ID:** ${fetching.id}\n**Bot:** ${fetching.bot ? "✅" : "❌"}\n**Joined At: <t:${Math.round(parseInt(fetching.createdTimestamp) / 1000)}:F>**`)
+                        .setTimestamp();
+                    return msg.channel.send({ embeds: [userInfo] });
+                } catch (err) {
+                    log(err);
+                    return
+                }
+            }
+        } catch (error) { log(error); }
     }
 };
