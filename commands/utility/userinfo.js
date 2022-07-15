0a4ef8d9d066fc646e30c05b20cba41e23665a88
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-16 00:51:01 +0200

    Update userinfo.js

--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -1,4 +1,5 @@
-const log = (arg) => console.log(arg);
+let log = (arg) => console.log(arg);
+
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
@@ -8,37 +9,31 @@ module.exports = {
     aliases: ['user', 'u-info', 'uinfo', 'user-info'],
     run: async (client, msg, args) => {
         try {
-            let member = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) || msg.guild.members.cache.find(member => member.user.username.toLowerCase() === args.slice(0).join(' ').toLowerCase());
-            if (member) {
+            let member = msg.mentions.members.first();
+            if (!member) {
                 try {
-                    const userInfo = new MessageEmbed()
-                        .setAuthor({ name: `${member.user.tag}`, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
-                        .setColor('BLUE')
-                        .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-                        .setTitle('User Info')
-                        .setDescription(`**Tag:** ${member.user.tag}\n**ID:** ${member.user.id}\n**Bot:** ${member.user.bot ? "✅" : "❌"}\n**Created At: <t:${Math.round(parseInt(member.user.createdTimestamp) / 1000)}:F>**\n**Joined At: <t:${Math.round(parseInt(member.joinedTimestamp) / 1000)}:F>**`)
-                        .setTimestamp();
-                    return msg.channel.send({ embeds: [userInfo] });
+                    member = await msg.guild.members.fetch(args[0]);
                 } catch (err) {
-                    log(err);
-                    return
+                    let fetching = await client.users.fetch(args[0]).then((fetching) => {
+                        let userInfo = new MessageEmbed()
+                            .setAuthor({ name: `${fetching.tag}`, iconURL: fetching.user.displayAvatarURL({ dynamic: true }) })
+                            .setColor('BLUE')
+                            .setThumbnail(fetching.displayAvatarURL({ dynamic: true }))
+                            .setTitle('User Info')
+                            .setDescription(`**Tag:** ${fetching.tag}\n**ID:** ${fetching.id}\n**Bot:** ${fetching.bot ? "✅" : "❌"}\n**Joined At: <t:${Math.round(parseInt(fetching.createdTimestamp) / 1000)}:F>**`)
+                            .setTimestamp();
+                        return msg.channel.send({ embeds: [userInfo] });
+                    });
                 }
-            } else {
-                try {
-                    let fetching = await client.users.fetch(args[0]);
-                    const userInfo = new MessageEmbed()
-                        .setAuthor({ name: `${fetching.tag}`, iconURL: fetching.displayAvatarURL({ dynamic: true }) })
-                        .setColor('BLUE')
-                        .setThumbnail(fetching.displayAvatarURL({ dynamic: true }))
-                        .setTitle('User Info')
-                        .setDescription(`**Tag:** ${fetching.tag}\n**ID:** ${fetching.id}\n**Bot:** ${fetching.bot ? "✅" : "❌"}\n**Joined At: <t:${Math.round(parseInt(fetching.createdTimestamp) / 1000)}:F>**`)
-                        .setTimestamp();
-                    return msg.channel.send({ embeds: [userInfo] });
-                } catch (err) {
-                    log(err);
-                    return
-                }
-            }
-        } catch (error) { log(error); }
-    }
-};
+            let userEmbed = new MessageEmbed()
+                .setAuthor({ name: `${member.tag}`, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
+            	.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+                .setColor('BLUE')
+                .setTitle('User Info')
+                .setDescription(`**Tag:** ${member.tag}\n**ID:** ${member.user.id}\n**Bot:** ${member.user.bot ? "✅" : "❌"}\n**Created At: <t:${Math.round(parseInt(member.user.createdTimestamp) / 1000)}:F>**\n**Joined At: <t:${Math.round(parseInt(member.joinedTimestamp) / 1000)}:F>**`)
+                .setTimestamp();
+            return msg.channel.send({ embeds: [userEmbed] });
+        }
+    } catch (error) { log(error); }
+}
+}
