0f5ca2a3280794fbd7c88e082682355db253c3ab
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-16 17:59:22 +0200

    Update userinfo.js

--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -1,4 +1,4 @@
-let log = (arg) => console.log(arg);
+const log = (arg) => console.log(arg);
 
 const { MessageEmbed } = require('discord.js');
 
@@ -6,34 +6,85 @@ module.exports = {
     name: 'userinfo',
     description: 'Gets information about a user.',
     usage: 'ds!userinfo <user>',
-    aliases: ['user', 'u-info', 'uinfo', 'user-info'],
+    aliases: ['user', 'u-info', 'uinfo', 'user-info', ' userinfo', ' user', ' u-info', ' uinfo', ' user-info'],
     run: async (client, msg, args) => {
         try {
             let member = msg.mentions.members.first();
-            if (!member) {
+            if (!args[0]) {
+                member = msg.member;
+                const authorEmbed = new MessageEmbed()
+                    .setAuthor({ name: `${member.user.tag}`, iconURL: member.displayAvatarURL({ dynamic: true }) })
+                    .setThumbnail(member.displayAvatarURL({ dynamic: true }))
+                    .setColor('BLUE')
+                    .setTitle('User Info')
+                    .addFields(
+                        { name: 'Tag', value: `${member.user.tag}`, inline: true },
+                        { name: 'ID', value: `${member.id}`, inline: true },
+                        { name: 'Bot', value: `${member.bot ? "✅" : "❌"}`, inline: true },
+                        { name: 'Created At', value: `**<t:${Math.round(parseInt(member.user.createdTimestamp) / 1000)}:F>**`, inline: true },
+                        { name: 'Joined At', value: `**<t:${Math.round(parseInt(member.joinedTimestamp) / 1000)}:F>**`, inline: true },
+                        { name: 'Roles', value: `${member.roles.cache.map(r => r.name).join(' ')}`, inline: false },
+                        { name: 'Admin', value: `${member.permissions.has('ADMINISTRATOR') ? "✅" : "❌"}`, inline: true }
+                    )
+                    .setTimestamp();
+                return msg.channel.send({ embeds: [authorEmbed] });
+            } else if (member) {
+                const memberEmbed = new MessageEmbed()
+                    .setAuthor({ name: `${member.user.tag}`, iconURL: member.displayAvatarURL({ dynamic: true }) })
+                    .setThumbnail(member.displayAvatarURL({ dynamic: true }))
+                    .setColor('BLUE')
+                    .setTitle('User Info')
+                    .addFields(
+                        { name: 'Tag', value: `${member.user.tag}`, inline: true },
+                        { name: 'ID', value: `${member.id}`, inline: true },
+                        { name: 'Bot', value: `${member.bot ? "✅" : "❌"}`, inline: true },
+                        { name: 'Created At', value: `**<t:${Math.round(parseInt(member.user.createdTimestamp) / 1000)}:F>**`, inline: true },
+                        { name: 'Joined At', value: `**<t:${Math.round(parseInt(member.joinedTimestamp) / 1000)}:F>**`, inline: true },
+                        { name: 'Roles', value: `${member.roles.cache.map(r => r.name).join(' ')}`, inline: false },
+                        { name: 'Admin', value: `${member.permissions.has('ADMINISTRATOR') ? "✅" : "❌"}`, inline: true }
+                    )
+                    .setTimestamp();
+                return msg.channel.send({ embeds: [memberEmbed] });
+            } else {
                 try {
                     member = await msg.guild.members.fetch(args[0]);
+                    const userEmbed = new MessageEmbed()
+                        .setAuthor({ name: `${member.user.tag}`, iconURL: member.displayAvatarURL({ dynamic: true }) })
+                        .setThumbnail(member.displayAvatarURL({ dynamic: true }))
+                        .setColor('BLUE')
+                        .setTitle('User Info')
+                        .addFields(
+                        { name: 'Tag', value: `${member.user.tag}`, inline: true },
+                        { name: 'ID', value: `${member.id}`, inline: true },
+                        { name: 'Bot', value: `${member.bot ? "✅" : "❌"}`, inline: true },
+                        { name: 'Created At', value: `**<t:${Math.round(parseInt(member.user.createdTimestamp) / 1000)}:F>**`, inline: true },
+                        { name: 'Joined At', value: `**<t:${Math.round(parseInt(member.joinedTimestamp) / 1000)}:F>**`, inline: true },
+                        { name: 'Roles', value: `${member.roles.cache.map(r => r.name).join(' ')}`, inline: false },
+                        { name: 'Admin', value: `${member.permissions.has('ADMINISTRATOR') ? "✅" : "❌"}`, inline: true }
+                    )
+                        .setTimestamp();
+                    return msg.channel.send({ embeds: [userEmbed] });
                 } catch (err) {
-                    let fetching = await client.users.fetch(args[0]).then((fetching) => {
-                        let userInfo = new MessageEmbed()
-                            .setAuthor({ name: `${fetching.tag}`, iconURL: fetching.user.displayAvatarURL({ dynamic: true }) })
+                    await client.users.fetch(args[0]).then((fetching) => {
+                        const userInfo = new MessageEmbed()
+                            .setAuthor({ name: `${fetching.tag}`, iconURL: fetching.displayAvatarURL({ dynamic: true }) })
                             .setColor('BLUE')
                             .setThumbnail(fetching.displayAvatarURL({ dynamic: true }))
                             .setTitle('User Info')
-                            .setDescription(`**Tag:** ${fetching.tag}\n**ID:** ${fetching.id}\n**Bot:** ${fetching.bot ? "✅" : "❌"}\n**Joined At: <t:${Math.round(parseInt(fetching.createdTimestamp) / 1000)}:F>**`)
+                            .addFields(
+                                { name: 'Tag', value: `${fetching.tag}`, inline: true },
+                                { name: 'ID', value: `${fetching.id}`, inline: true },
+                                { name: 'Bot', value: `${fetching.bot ? "✅" : "❌"}`, inline: true },
+                                { name: 'Created At', value: `**<t:${Math.round(parseInt(fetching.createdTimestamp) / 1000)}:F>**`, inline: true },
+                            )
                             .setTimestamp();
                         return msg.channel.send({ embeds: [userInfo] });
+                    }).catch((err) => {
+                        log(err)
+                        return msg.channel.send('User not found! Check your Input!');
                     });
                 }
-            let userEmbed = new MessageEmbed()
-                .setAuthor({ name: `${member.tag}`, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
-            	.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-                .setColor('BLUE')
-                .setTitle('User Info')
-                .setDescription(`**Tag:** ${member.tag}\n**ID:** ${member.user.id}\n**Bot:** ${member.user.bot ? "✅" : "❌"}\n**Created At: <t:${Math.round(parseInt(member.user.createdTimestamp) / 1000)}:F>**\n**Joined At: <t:${Math.round(parseInt(member.joinedTimestamp) / 1000)}:F>**`)
-                .setTimestamp();
-            return msg.channel.send({ embeds: [userEmbed] });
-        }
-    } catch (error) { log(error); }
-}
+            }
+        } catch (err) { log(err); }
+    }
 }
