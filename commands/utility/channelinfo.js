f21f770d4594882984fa56d5cd75c6e369696b2c
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-18 08:15:40 +0200

    Update channelinfo.js

--- commands/utility/channelinfo.js
+++ commands/utility/channelinfo.js
@@ -1,28 +1,29 @@
 const Discord = require('discord.js');
 const moment = require('moment');
 
+// initialize the channelinfo command
 module.exports = {
     name: 'channelinfo',
     description: 'Gets information about the channel.',
     usage: 'ds!channelinfo',
-    run: async(client, msg, args) => {
+    run: async (client, msg, args) => {
         try {
-            const toTimestamp = (strDate) => {
-                const dt = moment(strDate).unix();
-                return dt;
-            };
-            const channelinfo = new Discord.MessageEmbed()
+            let channel = msg.guild.channels.cache.get(args[0]) || msg.channel;
+            if (!channel) {
+                return msg.channel.send('You must specify a channel!');
+            }
+            let channelinfo = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`${msg.channel.name} Channel Information`)
-                .setThumbnail(msg.guild.iconURL({dynamic: true}))
+                .setThumbnail(msg.guild.iconURL({ dynamic: true }))
                 .addField('Channel Name', `${msg.channel.name}`)
                 .addField('Channel ID', `${msg.channel.id}`)
-                .addField('Channel Creation Date', `**<t:${toTimestamp(moment.utc(msg.channel.createdTimestamp))}:F>**`)
+                .addField('Channel Creation Date', `**<t:${Math.round(parseInt(msg.channel.createdTimestamp))}:F>**`)
                 .addField('Channel Type', `${msg.channel.type}`)
                 .addField('Channel Topic', `${msg.channel.topic}`)
                 .addField('Channel Position', `${msg.channel.position}`)
                 .setTimestamp();
-            return msg.channel.send({embeds: [channelinfo]});
+            return msg.channel.send({ embeds: [channelinfo] });
         } catch (error) {
             console.log(error);
             return msg.channel.send('An error occurred! Please try again!');
