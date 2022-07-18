e5234e668681d1ee7f30503110613b627ee79c9b
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-18 08:16:43 +0200

    Update math.js

--- commands/utility/math.js
+++ commands/utility/math.js
@@ -5,39 +5,39 @@ module.exports = {
     name: 'math',
     description: 'Evaluates a mathematical expression.',
     usage: 'ds!math <expression>',
-    run: async(client, message, args) => {
-        try{
+    run: async (client, message, args) => {
+        try {
             if (args.length < 1) {
-                message.channel.send('You need to provide some code!');
+                message.channel.send('Please provide an Math Expression:\n\nExample: **ds!math 2^3**\n**ds!math sqrt(4)**');
                 return;
             }
-            const input = args.join(' ');
-            const output = math.evaluate(input);
+            let input = args.join(' ');
+            let output = math.evaluate(input);
             if (isNaN(output)) {
-                const embed = new MessageEmbed()
-                .setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})})
-                .setColor('RED')
-                .setTitle('Math Error')
-                .setDescription(`**Math Expression:** \`\`\`js\n${input}\n\`\`\`\n**Output:**\`\`\`js\nNot a Number I guess... ¯\\_(ツ)_/¯\`\`\``)
-                .setTimestamp();
-                return message.reply({embeds: [embed]});
+                let embed = new MessageEmbed()
+                    .setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+                    .setColor('RED')
+                    .setTitle('Math Error')
+                    .setDescription(`**Math Expression:** \`\`\`js\n${input}\n\`\`\`\n**Result:**\`\`\`js\nNot a Number\`\`\``)
+                    .setTimestamp();
+                return message.reply({ embeds: [embed] });
             } else if (output === Infinity) {
-                const embed = new MessageEmbed()
-                .setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})})
-                .setColor('RED')
-                .setTitle('Math Error')
-                .setDescription(`**Math Expression:** \`\`\`js\n${input}\n\`\`\`\n**Output:**\`\`\`js\nInfinity\`\`\``)
-                .setTimestamp();
-                return message.reply({embeds: [embed]});
+                let embed = new MessageEmbed()
+                    .setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+                    .setColor('RED')
+                    .setTitle('Math Error')
+                    .setDescription(`**Math Expression:** \`\`\`js\n${input}\n\`\`\`\n**Result:**\`\`\`js\nInfinity\`\`\``)
+                    .setTimestamp();
+                return message.reply({ embeds: [embed] });
             }
-            const embed = new MessageEmbed()
-            .setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})})
-            .setColor('BLUE')
-            .setTitle('Math Result')
-            .setDescription(`**Math Expression:** \`\`\`js\n${input}\n\`\`\`\n**Result:**\`\`\`js\n${output}\`\`\``)
-            .setTimestamp();
-            return message.reply({embeds: [embed]});
-        } catch(error) {
+            let embed = new MessageEmbed()
+                .setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+                .setColor('BLUE')
+                .setTitle('Math Result')
+                .setDescription(`**Math Expression:** \`\`\`js\n${input}\n\`\`\`\n**Result:**\`\`\`js\n${output}\`\`\``)
+                .setTimestamp();
+            return message.reply({ embeds: [embed] });
+        } catch (error) {
             console.log(error);
         }
     }
