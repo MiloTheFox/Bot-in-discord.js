8973fe58daad73cb1bf54bac62f1f7c255e28be8
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-18 08:00:22 +0200

    Update facebook.js

--- commands/memes/facebook.js
+++ commands/memes/facebook.js
@@ -9,10 +9,10 @@ module.exports = {
     category: 'memes',
     run: async (client, message, args) => {
         try {
-            const url = await fetch("https://www.reddit.com/r/facebook/hot.json?limit=100").then(res => res.json());
-            const random = Math.floor(Math.random() * url.data.children.length);
-            const post = url.data.children[random].data;
-            const embed = new Discord.MessageEmbed()
+            let url = await fetch("https://www.reddit.com/r/facebook/hot.json?limit=100").then(res => res.json());
+            let random = Math.floor(Math.random() * url.data.children.length);
+            let post = url.data.children[random].data;
+            let embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`${post.title}`)
                 .setURL(`${post.url}`)
