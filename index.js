b398e5c728ed70cb9fdba060e54e2436c4db42d9
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-18 07:47:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,67 @@
-const { Client, Collection } = require('discord.js');
-const client = new Client({ 
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER', 'EMOJI', 'MESSAGE_REACTION', 'VOICE_STATE', 'WEBHOOK', 'INVITE', 'TYPING_START', 'PRESENCE'],
-    intents: 32767,
- });
-const config = require('./config.json');
-const fs = require('fs');
+const { Player } = require('discord-player');
+const { Client, Intents, Collection } = require('discord.js');
+const { readdirSync } = require('fs');
 
-module.exports = client;
+let client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ]
+});
 
+client.config = require('./config');
+client.player = new Player(client, client.config.opt.discordPlayer);
 client.commands = new Collection();
-client.aliases = new Collection();
-client.events = new Collection();
+let player = client.player
 
-client.on('ready', () => {
-    console.log(`${client.user.tag} is online!`);
-    client.user.setActivity(`${config.prefix}ping`, { type: 'PLAYING' });
+let events = readdirSync('./events/').filter(file => file.endsWith('.js'));
+for (const file of events) {
+    let event = require(`./events/${file}`);
+    console.log(`-> Loaded event ${file.split('.')[0]}`);
+    client.on(file.split('.')[0], event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+};
+console.log(`-> Loaded commands...`);
+readdirSync('./commands/').forEach(dirs => {
+    let commands = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+    for (const file of commands) {
+        let command = require(`./commands/${dirs}/${file}`);
+        console.log(`${command.name.toLowerCase()} Load Command!`);
+        client.commands.set(command.name.toLowerCase(), command);
+        delete require.cache[require.resolve(`./commands/${dirs}/${file}`)];
+    };
 });
 
-client.categories = fs.readdirSync('./commands');
 
-// load the files
-['command'].forEach(handler => {
-    require(`./handler/${handler}`)(client);
+player.on('trackStart', (queue, track) => {
+    if (!client.config.opt.loopMessage && queue.repeatMode !== 0) return;
+    queue.metadata.send({ content: `🎵 Music started playing: **${track.title}** -> Channel: **${queue.connection.channel.name}** 🎧` });
 });
-client.login(config.token);
+
+player.on('trackAdd', (queue, track) => {
+    queue.metadata.send({ content: `**${track.title}** added to playlist. ✅` });
+});
+
+player.on('botDisconnect', (queue) => {
+    queue.metadata.send({ content: 'Someone from the audio channel Im connected to kicked me out, the whole playlist has been cleared! ❌' });
+});
+
+player.on('channelEmpty', (queue) => {
+    queue.metadata.send({ content: 'I left the audio channel because there is no one on my audio channel. ❌' });
+});
+
+player.on('queueEnd', (queue) => {
+    queue.metadata.send({ content: 'All play queue finished, I think you can listen to some more music. ✅' });
+});
+
+
+
+if (client.config.TOKEN) {
+    client.login(client.config.TOKEN).catch(e => {
+        console.log("The Bot Token You Entered Into Your Project Is Incorrect Or Your Bot's INTENTS Are OFF!")
+    })
+} else {
+    console.log("Please Write Your Bot Token Opposite The Token In The config.js File In Your Project!")
+}
