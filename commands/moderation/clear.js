9d5dbfae9c243a480b03c76533bee546e74d8adc
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-22 01:56:06 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -5,19 +5,51 @@ module.exports = {
     aliases: ['purge', 'prune'],
     run: async (client, msg, args) => {
         try {
-            if (!msg.member.permissions.has('MANAGE_MESSAGES') && !msg.guild.me.permissions.has('MANAGE_MESSAGES')) {
-                return msg.channel.send('Missing Permission: `MANAGE_MESSAGES`').then(m => m.delete(3000));
+            let botperms = msg.channel.permissionsFor(msg.guild.me, false);
+            let userperms = msg.channel.permissionsFor(msg.author, false);
+            if (!botperms.has('MANAGE_MESSAGES')) {
+                return msg.channel.send('I do not have the `Manage Messages` permission.').then(m => {
+                    setTimeout(async () => {
+                        await m.delete();
+                    }, 5000);
+                });
             }
-            else if (!args[0]) return msg.channel.send('Please specify the amount of messages to delete!').then(m => m.delete(3000));
-            else if (isNaN(args[0])) return msg.channel.send('Please specify a valid number!').then(m => m.delete(3000));
-            else if (args[0] > 100) return msg.channel.send('Please specify a number less than 100!').then(m => m.delete(3000));
-            else if (args[0] < 1) return msg.channel.send('Please specify a number greater than 1!').then(m => m.delete(3000));
-            let amount = parseInt(args[0]);
+            if (!userperms.has('MANAGE_MESSAGES')) {
+                return msg.channel.send('You don\'t have the permission to delete messages.').then(m => {
+                    setTimeout(async () => {
+                        await m.delete();
+                    }, 5000);
+                });
+            }
+            if (!args[0]) {
+                return msg.channel.send('Please specify the amount of messages to delete!').then(m => {
+                    setTimeout(async () => {
+                        await m.delete();
+                    }, 5000);
+                });
+            }
+            else if (isNaN(args[0])) return msg.channel.send('Please specify a valid number!').then(m => {
+                setTimeout(async () => {
+                    await m.delete();
+                }, 5000);
+            });
+            else if (args[0] > 100) return msg.channel.send('Please specify a number less than 100!').then(m => {
+                setTimeout(async () => {
+                    await m.delete();
+                }, 5000);
+            });
+            else if (args[0] < 1) return msg.channel.send('Please specify a number greater than 1!').then(m => {
+                setTimeout(async () => {
+                    await m.delete();
+                }, 5000);
+            });
+            const amount = parseInt(args[0]);
             await msg.channel.bulkDelete(amount + 1);
-            return msg.channel.send(`Successfully deleted ${amount} messages!`).then(m => m.delete(3000));
+            return msg.channel.send(`Successfully deleted ${amount} messages!`).then(m => {
+                setTimeout(async () => { await m.delete(); }, 5000);
+            });
         } catch (error) {
             console.log(error);
-            return msg.channel.send('An error occurred! Please try again!');
         }
     }
 };
