636b410f245c5532a42d6fa119d9546edf1b9bb5
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-07-22 01:12:56 +0200

    Updated clear.js
    Deletes all individual responses sent by the Bot automatically after 3 seconds.

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -6,15 +6,15 @@ module.exports = {
     run: async (client, msg, args) => {
         try {
             if (!msg.member.permissions.has('MANAGE_MESSAGES') && !msg.guild.me.permissions.has('MANAGE_MESSAGES')) {
-                return msg.channel.send('Missing Permission: `MANAGE_MESSAGES`');
+                return msg.channel.send('Missing Permission: `MANAGE_MESSAGES`').then(m => m.delete(3000));
             }
-            if (!args[0]) return msg.channel.send('Please specify the amount of messages to delete!');
-            else if (isNaN(args[0])) return msg.channel.send('Please specify a valid number!');
-            else if (args[0] > 100) return msg.channel.send('Please specify a number less than 100!');
-            else if (args[0] < 1) return msg.channel.send('Please specify a number greater than 1!');
+            else if (!args[0]) return msg.channel.send('Please specify the amount of messages to delete!').then(m => m.delete(3000));
+            else if (isNaN(args[0])) return msg.channel.send('Please specify a valid number!').then(m => m.delete(3000));
+            else if (args[0] > 100) return msg.channel.send('Please specify a number less than 100!').then(m => m.delete(3000));
+            else if (args[0] < 1) return msg.channel.send('Please specify a number greater than 1!').then(m => m.delete(3000));
             let amount = parseInt(args[0]);
             await msg.channel.bulkDelete(amount + 1);
-            return msg.channel.send(`Successfully deleted ${amount} messages!`);
+            return msg.channel.send(`Successfully deleted ${amount} messages!`).then(m => m.delete(3000));
         } catch (error) {
             console.log(error);
             return msg.channel.send('An error occurred! Please try again!');
