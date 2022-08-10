cccc49a42a01836fe956f4c5de089d20b8c4bcd3
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-08-11 01:50:13 +0200

    Update timeout.js

--- commands/moderation/timeout.js
+++ commands/moderation/timeout.js
@@ -9,31 +9,28 @@ module.exports = {
         try {
             let time = args[1];
             let reason = args.slice(2).join(' ') || 'No reason specified!';
-            let milliseconds = ms(time);
             let user = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) || await msg.guild.members.fetch(args[0]);
-            if (!time) {
-                return msg.channel.send('You must specify a time!');
+            if (!time || !user) {
+                return msg.channel.send('You must specify a user and the time!');
             }
-            if (!user) {
-                return msg.channel.send('You must specify a user!');
-            }
-            if (user.permissions.has('ADMINISTRATOR')) {
+            let milliseconds = ms(time);
+            else if (user.permissions.has('ADMINISTRATOR')) {
                 return msg.channel.send('You do not have the permission to timeout this user as they are an Administrator!');
             }
-            if (!msg.guild.me.permissions.has('MODERATE_MEMBERS')) {
+            else if (!msg.guild.me.permissions.has('MODERATE_MEMBERS')) {
                 return msg.channel.send('I do not have the permission to timeout this user!');
             }
-            if (user.id === msg.guild.ownerId) {
+            else if (user.id === msg.guild.ownerId) {
                 return msg.channel.send('You cannot timeout the owner of the server!');
             }
-            if (user.id === client.user.id) {
+            else if (user.id === client.user.id) {
                 return msg.channel.send('I cannot timeout myself!');
             }
-            if (milliseconds > 28 * 24 * 60 * 60 * 1000) {
+            else if (milliseconds > 28 * 24 * 60 * 60 * 1000) {
                 return msg.channel.send('The timeout limit is 28 Days due to Discord Limitations!');
             }
-            if (!user.isCommunicationDisabled()) {
-            await user.timeout(milliseconds, reason);
+            else if (!user.isCommunicationDisabled()) {
+            await user.timeout(ms(time), reason);
             let embed = new Discord.MessageEmbed()
                 .setAuthor({ name: `${msg.author.tag}`, iconURL: msg.author.displayAvatarURL({ dynamic: true }) })
                 .setColor('BLUE')
